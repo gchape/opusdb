99003586f1927067688289c285a40e6581ccc8d0
Giorgi Chapidze <chapidze.giorgi@proton.me>
2025-12-04 23:38:46 +0400

    Initialize OpusDB Clojure project with Leiningen scaffold

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject opusdb "0.1.0-SNAPSHOT"
+  :description "OpusDB: a lightweight, in-memory database written in Clojure for fast, functional data management."
+  :url "https://github.com/gchape/opusdb"
+  :license {:name "EPL-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.12.2"]]
+  :main ^:skip-aot opusdb.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
---
+++ src/opusdb/core.clj
@@ -0,0 +1,6 @@
+(ns opusdb.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& _])
