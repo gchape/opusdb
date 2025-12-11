f7d5e1b1d750149f125c1a3dfb5feefe88e25c92
Giorgi Chapidze <chapidze.giorgi@proton.me>
2025-12-12 00:47:17 +0400

    Bump Clojure version to 1.12.4

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/gchape/opusdb"
   :license {:name "EPL-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.12.2"]]
+  :dependencies [[org.clojure/clojure "1.12.4"]]
   :main opusdb.main.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
