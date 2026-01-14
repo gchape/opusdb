5db3b25db0eecf1a1845e9379abc7f5036bac17c
Giorgi Chapidze <chapidze.giorgi@proton.me>
2026-01-14 10:14:12 +0400

    (ns opusdb.cache.splay   (:refer-clojure :exclude [get type remove vals]))
    (defrecord Cache [tree size type eviction-fn count])
    
    (defn- rotate-right [{left :left :as node}]
      (if left (assoc left :right (assoc node :left (:right left))) node))
    
    (defn- rotate-left [{right :right :as node}]
      (if right (assoc right :left (assoc node :right (:left right))) node))
    
    (defn- leaf? [{:keys [left right]}]
      (and (nil? left) (nil? right)))
    
    (defn- leftmost-key [{left :left key :key}]
      (if left (recur left) key))
    
    (defn- leftmost-node [{left :left :as node}]
      (if left (recur left) node))
    
    (defn- splay [node key]
      (when node
        (let [cmp (compare key (:key node))]
          (cond
            (zero? cmp) node
            (neg? cmp)
            (if-let [left (:left node)]
              (let [cmp-left (compare key (:key left))]
                (cond
                  (neg? cmp-left) (-> node (assoc :left (splay (:left left) key)) rotate-right rotate-right)
                  (pos? cmp-left) (-> node (assoc :left (splay (:right left) key)) rotate-left rotate-right)
                  :else (rotate-right node)))
              node)
            :else
            (if-let [right (:right node)]
              (let [cmp-right (compare key (:key right))]
                (cond
                  (pos? cmp-right) (-> node (assoc :right (splay (:right right) key)) rotate-left rotate-left)
                  (neg? cmp-right) (-> node (assoc :right (splay (:left right) key)) rotate-right rotate-left)
                  :else (rotate-left node)))
              node)))))
    
    (defn- remove [node cache-type]
      (case cache-type
        "LRU" (if-let [left (:left node)]
                (assoc node :left (remove left cache-type))
                (:right node))
        "MRU" (cond
                (nil? node) nil
                (leaf? node) nil
                (nil? (:left node)) (:right node)
                (nil? (:right node)) (:left node)
                :else (assoc (splay (:right node) (leftmost-key (:right node))) :left (:left node)))))
    
    (defn- insert [node key val]
      (if node
        (let [cmp (compare key (:key node))]
          (cond
            (neg? cmp) (assoc node :left (insert (:left node) key val))
            (pos? cmp) (assoc node :right (insert (:right node) key val))
            :else (assoc node :value val)))
        {:key key :value val}))
    
    (defn- contains-key? [node key]
      (when node
        (let [cmp (compare key (:key node))]
          (cond
            (zero? cmp) true
            (neg? cmp) (recur (:left node) key)
            :else (recur (:right node) key)))))
    
    (defn put! [{:keys [tree size type eviction-fn count]} k v]
      (let [root @tree
            existing? (contains-key? root k)
            full? (and (>= @count size) (not existing?))]
        (when full?
          (let [victim (if (= type "LRU") (leftmost-node root) root)]
            (when eviction-fn (eviction-fn (:key victim) (:value victim)))))
        (when-not existing? (swap! count inc))
        (reset! tree (splay (insert (if full? (remove root type) root) k v) k))))
    
    (defn get [{:keys [tree]} k]
      (when-let [root (splay @tree k)]
        (reset! tree root)
        (loop [node root]
          (when node
            (let [cmp (compare k (:key node))]
              (cond
                (zero? cmp) (:value node)
                (neg? cmp) (recur (:left node))
                :else (recur (:right node))))))))
    
    (defn vals [{:keys [tree]}]
      (letfn [(walk [node]
                (when node (concat (walk (:left node)) [(:value node)] (walk (:right node)))))]
        (vec (walk @tree))))
    
    (defn make-cache
      ([size eviction-fn] (make-cache size eviction-fn "LRU"))
      ([size eviction-fn cache-type] (->Cache (atom nil) size cache-type eviction-fn (atom 0))))

--- src/opusdb/cache/splay.clj
+++ src/opusdb/cache/splay.clj
@@ -3,131 +3,98 @@
 
 (defrecord Cache [tree size type eviction-fn count])
 
-(defn- make-node [k v]
-  {:key k :value v})
+(defn- rotate-right [{left :left :as node}]
+  (if left (assoc left :right (assoc node :left (:right left))) node))
 
-(defn- rotate-right [{l :left :as t}]
-  (if l
-    (assoc l :right (assoc t :left (:right l)))
-    t))
+(defn- rotate-left [{right :right :as node}]
+  (if right (assoc right :left (assoc node :right (:left right))) node))
 
-(defn- rotate-left [{r :right :as t}]
-  (if r
-    (assoc r :left (assoc t :right (:left r)))
-    t))
-
-(defn- is-leaf? [{:keys [left right]}]
+(defn- leaf? [{:keys [left right]}]
   (and (nil? left) (nil? right)))
 
-(defn- leftmost-key [{l :left k :key}]
-  (if l (recur l) k))
+(defn- leftmost-key [{left :left key :key}]
+  (if left (recur left) key))
 
-(defn- leftmost-node [{l :left :as t}]
-  (if l (recur l) t))
+(defn- leftmost-node [{left :left :as node}]
+  (if left (recur left) node))
 
-(defn- splay [t k]
-  (cond
-    (nil? t) nil
-    (zero? (compare k (:key t))) t
-    (neg? (compare k (:key t)))
-    (let [l (:left t)]
+(defn- splay [node key]
+  (when node
+    (let [cmp (compare key (:key node))]
       (cond
-        (nil? l) t
-        (neg? (compare k (:key l)))
-        (-> t
-            (assoc :left (splay (:left l) k))
-            rotate-right
-            rotate-right)
-        (pos? (compare k (:key l)))
-        (-> t
-            (assoc :left (splay (:right l) k))
-            rotate-left
-            rotate-right)
-        :else (rotate-right t)))
-    :else
-    (let [r (:right t)]
-      (cond
-        (nil? r) t
-        (pos? (compare k (:key r)))
-        (-> t
-            (assoc :right (splay (:right r) k))
-            rotate-left
-            rotate-left)
-        (neg? (compare k (:key r)))
-        (-> t
-            (assoc :right (splay (:left r) k))
-            rotate-right
-            rotate-left)
-        :else (rotate-left t)))))
+        (zero? cmp) node
+        (neg? cmp)
+        (if-let [left (:left node)]
+          (let [cmp-left (compare key (:key left))]
+            (cond
+              (neg? cmp-left) (-> node (assoc :left (splay (:left left) key)) rotate-right rotate-right)
+              (pos? cmp-left) (-> node (assoc :left (splay (:right left) key)) rotate-left rotate-right)
+              :else (rotate-right node)))
+          node)
+        :else
+        (if-let [right (:right node)]
+          (let [cmp-right (compare key (:key right))]
+            (cond
+              (pos? cmp-right) (-> node (assoc :right (splay (:right right) key)) rotate-left rotate-left)
+              (neg? cmp-right) (-> node (assoc :right (splay (:left right) key)) rotate-right rotate-left)
+              :else (rotate-left node)))
+          node)))))
 
-(defn- remove [t cache-type]
+(defn- remove [node cache-type]
   (case cache-type
-    "LRU"
-    (if-let [l (:left t)]
-      (assoc t :left (remove l cache-type))
-      (:right t))
-    "MRU"
-    (cond
-      (nil? t) nil
-      (is-leaf? t) nil
-      (nil? (:left t)) (:right t)
-      (nil? (:right t)) (:left t)
-      :else (assoc (splay (:right t) (leftmost-key (:right t)))
-                   :left (:left t)))))
+    "LRU" (if-let [left (:left node)]
+            (assoc node :left (remove left cache-type))
+            (:right node))
+    "MRU" (cond
+            (nil? node) nil
+            (leaf? node) nil
+            (nil? (:left node)) (:right node)
+            (nil? (:right node)) (:left node)
+            :else (assoc (splay (:right node) (leftmost-key (:right node))) :left (:left node)))))
 
-(defn- put [t k v]
-  (cond
-    (nil? t) (make-node k v)
-    (neg? (compare k (:key t))) (assoc t :left (put (:left t) k v))
-    (pos? (compare k (:key t))) (assoc t :right (put (:right t) k v))
-    :else (assoc t :value v)))
+(defn- insert [node key val]
+  (if node
+    (let [cmp (compare key (:key node))]
+      (cond
+        (neg? cmp) (assoc node :left (insert (:left node) key val))
+        (pos? cmp) (assoc node :right (insert (:right node) key val))
+        :else (assoc node :value val)))
+    {:key key :value val}))
 
-(defn- key-exists? [t k]
-  (loop [node t]
-    (cond
-      (nil? node) false
-      (neg? (compare k (:key node))) (recur (:left node))
-      (pos? (compare k (:key node))) (recur (:right node))
-      :else true)))
+(defn- contains-key? [node key]
+  (when node
+    (let [cmp (compare key (:key node))]
+      (cond
+        (zero? cmp) true
+        (neg? cmp) (recur (:left node) key)
+        :else (recur (:right node) key)))))
 
 (defn put! [{:keys [tree size type eviction-fn count]} k v]
-  (let [t @tree
-        current-count @count
-        existing? (key-exists? t k)]
-    (if (and (>= current-count size) (not existing?))
-      ;; Cache is full and this is a new key - need to evict
-      (let [victim (if (= type "LRU") (leftmost-node t) t)
-            t' (remove t type)]
-        (when eviction-fn
-          (eviction-fn (:key victim) (:value victim)))
-        (reset! tree (splay (put t' k v) k)))
-      ;; Cache has space or we're updating existing key
-      (do
-        (when-not existing?
-          (swap! count inc))
-        (reset! tree (splay (put t k v) k))))))
+  (let [root @tree
+        existing? (contains-key? root k)
+        full? (and (>= @count size) (not existing?))]
+    (when full?
+      (let [victim (if (= type "LRU") (leftmost-node root) root)]
+        (when eviction-fn (eviction-fn (:key victim) (:value victim)))))
+    (when-not existing? (swap! count inc))
+    (reset! tree (splay (insert (if full? (remove root type) root) k v) k))))
 
 (defn get [{:keys [tree]} k]
-  (when-let [t (splay @tree k)]
-    (reset! tree t)
-    (loop [node t]
-      (cond
-        (nil? node) nil
-        (neg? (compare k (:key node))) (recur (:left node))
-        (pos? (compare k (:key node))) (recur (:right node))
-        :else (:value node)))))
+  (when-let [root (splay @tree k)]
+    (reset! tree root)
+    (loop [node root]
+      (when node
+        (let [cmp (compare k (:key node))]
+          (cond
+            (zero? cmp) (:value node)
+            (neg? cmp) (recur (:left node))
+            :else (recur (:right node))))))))
 
 (defn vals [{:keys [tree]}]
-  (letfn [(walk [t]
-            (when t
-              (concat
-               (walk (:left t))
-               [(:value t)]
-               (walk (:right t)))))]
+  (letfn [(walk [node]
+            (when node (concat (walk (:left node)) [(:value node)] (walk (:right node)))))]
     (vec (walk @tree))))
 
 (defn make-cache
-  ([size eviction-fn]
-   (make-cache size eviction-fn "LRU"))
-  ([size eviction-fn cache-type]
-   (->Cache (atom nil) size cache-type eviction-fn (atom 0))))
+  ([size eviction-fn] (make-cache size eviction-fn "LRU"))
+  ([size eviction-fn cache-type] (->Cache (atom nil) size cache-type eviction-fn (atom 0))))
