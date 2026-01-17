053aaa7848f8098ced11d25f0881b1b47fa67b0a
Giorgi Chapidze <chapidze.giorgi@proton.me>
2026-01-17 09:12:20 +0400

    Refactor STM: extract process-result function
    Extract transaction result handling into process-result function for
    better code organization. Fixes NullPointerException by ensuring
    transaction ID exists before rollback. Maintains *current-transaction*
    binding correctly across retry iterations.
    
    - Add process-result function to handle :ok, :retry, :abort cases
    - Use Collections/binarySearch instead of custom binary search
    - Improve variable naming and add type hints
    - Simplify run loop with threading macro

--- src/opusdb/atomic/stm.clj
+++ src/opusdb/atomic/stm.clj
@@ -1,68 +1,68 @@
 (ns opusdb.atomic.stm
-   (:refer-clojure :exclude [ref deref ref-set alter dosync sync])
-   (:require
-    [opusdb.atomic.txcb :as txcb])
-   (:import
-    [java.util HashMap]
-    [java.util.concurrent ConcurrentHashMap]
-    [java.util.concurrent.atomic AtomicInteger AtomicLong]))
+  (:refer-clojure :exclude [ref deref ref-set alter dosync sync])
+  (:require
+   [opusdb.atomic.txcb :as txcb])
+  (:import
+   [java.util Collections HashMap]
+   [java.util.concurrent ConcurrentHashMap]
+   [java.util.concurrent.atomic AtomicInteger AtomicLong]))
 
- (def ^:private MAX_HISTORY 16)
- (def ^:private COMMIT_LOCK (Object.))
- (def ^:private ^AtomicLong WRITE_POINT (AtomicLong.))
- (def ^:private ^AtomicLong TRANSACTION_ID (AtomicLong.))
- (def ^:private ^ConcurrentHashMap ACTIVE_TRANSACTIONS (ConcurrentHashMap.))
+(def ^:private MAX_HISTORY 16)
+(def ^:private COMMIT_LOCK (Object.))
+(def ^:private ^AtomicLong WRITE_POINT (AtomicLong.))
+(def ^:private ^AtomicLong TRANSACTION_ID (AtomicLong.))
+(def ^:private ^ConcurrentHashMap ACTIVE_TRANSACTIONS (ConcurrentHashMap.))
 
- (def ^{:private true :dynamic true} *current-transaction* nil)
+(def ^{:private true :dynamic true} *current-transaction* nil)
 
- (defn- make-transaction []
-   (let [read-point (.get WRITE_POINT)
-         tx-id (.incrementAndGet TRANSACTION_ID)
-         tx {:id tx-id
-             :read-set (HashMap.)
-             :write-set (HashMap.)
-             :read-point read-point
-             :retry-count (AtomicInteger.)
-             :status (volatile! ::RUNNING)}]
-     (.put ACTIVE_TRANSACTIONS tx-id tx)
-     tx))
+(defn- make-transaction []
+  (let [read-point (.get WRITE_POINT)
+        tx-id (.incrementAndGet TRANSACTION_ID)
+        tx {:id tx-id
+            :read-set (HashMap.)
+            :write-set (HashMap.)
+            :read-point read-point
+            :retry-count (AtomicInteger.)
+            :status (volatile! ::RUNNING)}]
+    (.put ACTIVE_TRANSACTIONS tx-id tx)
+    tx))
 
- (defn- retry []
-   (throw (ex-info "Transaction retry" {:type ::retry})))
+(defn- retry []
+  (throw (ex-info "Transaction retry" {:type ::retry})))
 
- (defn- abort-if-retrying [tx]
-   (when (= @(:status tx) ::RETRY)
-     (retry)))
+(defn- abort-if-retrying [tx]
+  (when (= @(:status tx) ::RETRY)
+    (retry)))
 
- (defn- ensure-read-consistency [read-set]
-   (doseq [[ref entry] read-set]
-     (when (> (:write-point @ref) (:write-point entry))
-       (retry))))
+(defn- ensure-read-consistency [read-set]
+  (doseq [[ref entry] read-set]
+    (when (> (:write-point @ref) (:write-point entry))
+      (retry))))
 
- (defn- apply-writes! [write-set]
-   (let [wp (.incrementAndGet WRITE_POINT)]
-     (doseq [[ref val] write-set]
-       (swap! ref
-              (fn [{:keys [history] :as state}]
-                (let [h' (->> (conj history {:value val :write-point wp})
-                              (take-last MAX_HISTORY)
-                              vec)]
-                  (merge state {:history h'
-                                :write-point wp
-                                :owner nil})))))))
+(defn- apply-writes! [write-set]
+  (let [write-point' (.incrementAndGet WRITE_POINT)]
+    (doseq [[ref val] write-set]
+      (swap! ref
+             (fn [{:keys [history] :as state}]
+               (let [history' (->> (conj history
+                                         {:value val :write-point write-point'})
+                                   (take-last MAX_HISTORY)
+                                   vec)]
+                 (merge state {:history history'
+                               :write-point write-point'
+                               :owner nil})))))))
 
 (defn- find-version [read-point history]
-  (loop [lo 0
-         hi (dec (count history))
-         result nil]
-    (if (<= lo hi)
-      (let [mid (quot (+ lo hi) 2)
-            {:keys [write-point] :as entry} (nth history mid)]
-        (cond
-          (= write-point read-point) entry
-          (< write-point read-point) (recur (inc mid) hi entry)
-          :else (recur lo (dec mid) result)))
-      result)))
+  (let [i (Collections/binarySearch history
+                                    {:write-point read-point}
+                                    #(compare
+                                      (:write-point %1)
+                                      (:write-point %2)))]
+    (if (>= i 0)
+      (history i)
+      (let [i (- (inc i))]
+        (when (> i 0)
+          (history (dec i)))))))
 
 (defn- acquire-ownership [ref tx]
   (let [lock (:lock @ref)]
@@ -81,6 +81,29 @@
           false))
       (finally
         (monitor-exit lock)))))
+
+(defn- process-result [tx result]
+  (cond
+    (:ok result)
+    (:ok result)
+
+    (:retry result)
+    (do
+      (txcb/rollback! (:id tx))
+
+      (let [n (.get ^AtomicInteger (:retry-count tx))]
+        (when (pos? n)
+          (Thread/sleep (bit-shift-left 1 (min n 5)))))
+
+      (let [tx' (make-transaction)]
+        (.set ^AtomicInteger (:retry-count tx')
+              (.incrementAndGet ^AtomicInteger (:retry-count tx)))
+        tx'))
+
+    (:abort result)
+    (do
+      (txcb/rollback! (:id tx))
+      (throw (:abort result)))))
 
 (defn- commit [tx]
   (abort-if-retrying tx)
@@ -101,40 +124,23 @@
 (defn- run [tx fun]
   (loop [tx tx]
     (let [result
-          (try
-            (binding [*current-transaction* tx]
-              {:ok (let [r (fun)]
-                     (commit tx)
-                     r)})
+          (->> (try
+                 (binding [*current-transaction* tx]
+                   {:ok (let [r (fun)]
+                          (commit tx)
+                          r)})
 
-            (catch clojure.lang.ExceptionInfo e
-              (if (= (:type (ex-data e)) ::retry)
-                {:retry true}
-                (throw e)))
+                 (catch clojure.lang.ExceptionInfo e
+                   (if (= (:type (ex-data e)) ::retry)
+                     {:retry true}
+                     (throw e)))
 
-            (catch Throwable t
-              {:abort t}))]
-      (cond
-        (:ok result)
-        (:ok result)
-
-        (:retry result)
-        (do
-          (txcb/rollback! (:id tx))
-          
-          (let [n (.get (:retry-count tx))]
-            (when (pos? n)
-              (Thread/sleep (bit-shift-left 1 (min n 5)))))
-
-          (let [tx' (make-transaction)]
-            (.set (:retry-count tx')
-                  (.incrementAndGet (:retry-count tx)))
-            (recur tx')))
-
-        (:abort result)
-        (do
-          (txcb/rollback! (:id tx))
-          (throw (:abort result)))))))
+                 (catch Throwable t
+                   {:abort t}))
+               (process-result tx))]
+      (if (map? result)
+        (recur result)
+        result))))
 
 (defn sync [fun]
   (if *current-transaction*
@@ -145,10 +151,10 @@
   `(sync (fn* [] ~@body)))
 
 (defn ref [val]
-  (let [wp (.get WRITE_POINT)]
+  (let [write-point (.get WRITE_POINT)]
     (atom {:owner nil
-           :write-point wp
-           :history [{:value val :write-point wp}]
+           :write-point write-point
+           :history [{:value val :write-point write-point}]
            :lock (Object.)})))
 
 (defn deref [ref]
