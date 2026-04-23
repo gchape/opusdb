61409de5221ba072bfad94c44d7242c517d03bc9
Giorgi Chapidze <chapidze.giorgi@proton.me>
2026-04-23 14:59:47 +0400

    chore: remove benchmarking code from main (relocated to benchmark branch)
    - Removed bank and throughput benchmark modules
    - Main branch now contains only STM core implementation
    - Benchmarking code preserved in feature/benchmark branch

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "EPL-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.12.4"]]
-  :main opusdb.main.core
+  :main opusdb.main
   :target-path "target/%s"
   :profiles {:dev {:dependencies [[criterium "0.4.6"]]
                    :jvm-opts ["-XX:+UnlockDiagnosticVMOptions"
